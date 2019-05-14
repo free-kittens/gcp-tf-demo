f306a4eb92f7c482a207568d41fb817df67c9bbe
Tim <50206052+free-kittens@users.noreply.github.com>
2019-05-14 12:21:37 +0200

    Create main.tf
    main.tf file for terraform in GCP via Cloud Shell

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "google" {
+ project     = "possible-jetty-239813"
+ region      = "europe-west1"
+}
+
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+resource "google_compute_instance" "free-kittens" {
+ name = "free-kittens-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone = "europe-west1-b"
+
+ boot_disk {
+  initialize_params {
+   image = "gce-uefi-images/centos-7"
+  }
+ }
+
+ metadata_startup_script = "sudo yum -q -y update; sudo yum install epel-release; sudo yum install nginx"
+
+ network_interface {
+  network = "default"
+
+  access_config {
+  }
+ }
+
+ metadata {
+  sshKeys = "panther:${file("id_ed25519.pub")}"
+ }
+}
+
+
+resource  "google_compute_firewall" "default" {
+ name    = "nginx-firewall"
+ network = "default"
+
+allow {
+ protocol = "tcp"
+ ports = ["80","443"]
+}
+allow {
+ protocol = "icmp"
+}
+}
+
+output "ip" {
+ value = "${google_compute_instance.free-kittens.network_interface.0.access_config.0.nat_ip}"
+}
